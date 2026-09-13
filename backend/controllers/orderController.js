b47e4b9a4924d8bcd9370d00f766639c34a4cda5
Abhiraj1322 <abhirajdogra2@gmail.com>
2026-09-12 21:17:12 -0400

    orderRoutes for user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -68,8 +68,23 @@ const getOrderById = async (req, res) => {
   }
 };
 
+ const getUserOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user._id })
+      .populate("items.productId")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({ success: true, orders });
+  } catch (error) {
+    console.error("Error fetching user orders:", error);
+    res.status(500).json({ success: false, message: "Server error fetching orders" });
+  }
+};
+
+
 // CommonJS module export
 module.exports = {
   createOrder,
   getOrderById,
+  getUserOrders
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getOrderById } = require("../controllers/orderController");
+const { createOrder, getOrderById,getUserOrders } = require("../controllers/orderController");
 const { protect } = require("../middleware/authMiddleware"); // Your auth middleware
 
 router.post("/", protect, createOrder);
+router.get("/my-orders", protect, getUserOrders); // MUST be above /:id
 router.get("/:id", protect, getOrderById);
 
 module.exports = router;
