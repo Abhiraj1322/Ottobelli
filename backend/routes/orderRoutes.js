a1570c29905a7e3aee557716902f99ff3bf9e811
Abhiraj1322 <abhirajdogra2@gmail.com>
2026-09-15 21:24:25 -0400

    orderRoutes for user

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getOrderById,getUserOrders,  getAllOrders,
-  updateOrderStatus, } = require("../controllers/orderController");
+const { createOrder,getUserOrders,  getAllOrders,
+  updateOrderStatus,getOrderById } = require("../controllers/orderController");
 const { protect } = require("../middleware/authMiddleware"); // Your auth middleware
 
 router.post("/", protect, createOrder);
 router.get("/my-orders", protect, getUserOrders); // MUST be above /:id
-router.get("/:id", protect, getOrderById);
+
 router.get("/all",protect,getAllOrders);
 router.put("/:id/status",protect,updateOrderStatus)
+router.get("/:id", protect, getOrderById);
 module.exports = router;
